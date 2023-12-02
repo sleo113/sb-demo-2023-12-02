9f9cc5b79ffbc62dde90bb8d6f27b8b25a3f9c0d
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-02 15:01:56 +0900

    Add / , /about

--- src/main/java/com/ll/demo2312021/HomeController.java
+++ src/main/java/com/ll/demo2312021/HomeController.java
@@ -1,7 +1,22 @@
 package com.ll.demo2312021;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    String start(){
+        return "안녕하세요";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String startAAbout(){
+        return "개발자 커뮤니티!";
+    }
+
 }
