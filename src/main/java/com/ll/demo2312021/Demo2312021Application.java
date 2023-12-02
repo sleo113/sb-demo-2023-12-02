a1649f596924277d8183854ea5c44fe7ec5d0457
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-02 14:33:28 +0900

    setting

---
+++ src/main/java/com/ll/demo2312021/Demo2312021Application.java
@@ -0,0 +1,13 @@
+package com.ll.demo2312021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2312021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2312021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/demo2312021/Demo2312021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.demo2312021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2312021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
